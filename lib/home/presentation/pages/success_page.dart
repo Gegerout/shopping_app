60cb1456f6039639b0c61990d106ab32e1fe72ec
Gegerout <selimchik06@gmail.com>
2023-06-18 20:38:45 +0300

    home screen update

---
+++ lib/home/presentation/pages/success_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SuccessPage extends StatelessWidget {
+  const SuccessPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Stack(
+          children: [
+            Image.asset("assets/images/success_image.png")
+          ],
+        ),
+      ),
+    );
+  }
+}
