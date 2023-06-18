01bafc072ed25bfffaf45ab4f22a8b4eb43ba586
Gegerout <selimchik06@gmail.com>
2023-06-18 19:08:59 +0300

    home screen update

--- lib/home/presentation/pages/buy_page.dart
+++ lib/home/presentation/pages/buy_page.dart
@@ -22,7 +22,7 @@ class BuyPage extends ConsumerStatefulWidget {
 }
 
 class _BuyPageState extends ConsumerState<BuyPage> {
-  int currentPage = 0;
+  int currentPage = 2;
 
   final screens = [
     HomeWidget(),
@@ -37,18 +37,18 @@ class _BuyPageState extends ConsumerState<BuyPage> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        leading: Padding(
+        leading: IconButton(
           padding: const EdgeInsets.only(left: 20),
-          child: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: const Icon(
-                Icons.arrow_circle_left_outlined,
-                color: Color(0xFFF1C40F),
-                size: 36,
-              )),
-        ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: SizedBox(
+              width: 38,
+                height: 38,
+                child: Image.asset(
+              "assets/images/arrow-left-circle.png",
+            )),
+          ),
         title: Text(
           "Shop",
           style: GoogleFonts.montserrat(
@@ -59,9 +59,8 @@ class _BuyPageState extends ConsumerState<BuyPage> {
         centerTitle: true,
         actions: [
           Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: Image.asset("assets/images/search_big.png")
-          )
+              padding: const EdgeInsets.only(right: 20),
+              child: Image.asset("assets/images/search_big.png"))
         ],
       ),
       body: Padding(
@@ -69,7 +68,9 @@ class _BuyPageState extends ConsumerState<BuyPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const SizedBox(height: 30,),
+            const SizedBox(
+              height: 30,
+            ),
             Center(
               child: Material(
                 borderRadius: BorderRadius.circular(10),
@@ -90,14 +91,15 @@ class _BuyPageState extends ConsumerState<BuyPage> {
               style: GoogleFonts.montserrat(
                   fontWeight: FontWeight.w500, fontSize: 15),
             ),
-            const SizedBox(height: 18,),
+            const SizedBox(
+              height: 18,
+            ),
             Container(
               width: 83,
               height: 25,
               decoration: BoxDecoration(
-                color: const Color(0xFFC0C0C0).withOpacity(0.35),
-                borderRadius: BorderRadius.circular(20)
-              ),
+                  color: const Color(0xFFC0C0C0).withOpacity(0.35),
+                  borderRadius: BorderRadius.circular(20)),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -106,7 +108,7 @@ class _BuyPageState extends ConsumerState<BuyPage> {
                     child: IconButton(
                         padding: EdgeInsets.zero,
                         onPressed: () {
-                          if(ref.watch(buyProvider).count != 1) {
+                          if (ref.watch(buyProvider).count != 1) {
                             ref.read(buyProvider.notifier).decrement();
                           }
                         },
@@ -119,12 +121,20 @@ class _BuyPageState extends ConsumerState<BuyPage> {
                   const SizedBox(
                     width: 1,
                   ),
-                  Text("${ref.watch(buyProvider).count} Kg", style: GoogleFonts.roboto(fontWeight: FontWeight.w400, fontSize: 15, color: const Color(0xFFF1C40F)),),
-                  const SizedBox(width: 1,),
+                  Text(
+                    "${ref.watch(buyProvider).count} Kg",
+                    style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 15,
+                        color: const Color(0xFFF1C40F)),
+                  ),
+                  const SizedBox(
+                    width: 1,
+                  ),
                   SizedBox(
                     width: 24,
                     child: IconButton(
-                      padding: EdgeInsets.zero,
+                        padding: EdgeInsets.zero,
                         onPressed: () {
                           ref.read(buyProvider.notifier).increment();
                         },
@@ -137,8 +147,16 @@ class _BuyPageState extends ConsumerState<BuyPage> {
                 ],
               ),
             ),
-            const SizedBox(height: 20,),
-            Text("\$${int.parse(widget.price) * ref.watch(buyProvider).count} US", style: GoogleFonts.roboto(fontWeight: FontWeight.w700, fontSize: 32, color: const Color(0xFFF1C40F)),),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "\$${int.parse(widget.price) * ref.watch(buyProvider).count} US",
+              style: GoogleFonts.roboto(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 32,
+                  color: const Color(0xFFF1C40F)),
+            ),
             const Spacer(),
             Padding(
               padding: const EdgeInsets.only(bottom: 20),
@@ -174,7 +192,12 @@ class _BuyPageState extends ConsumerState<BuyPage> {
                   setState(() {
                     currentPage = 0;
                   });
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(page: 0,)));
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => HomePage(
+                                page: 0,
+                              )));
                 },
                 child: Image.asset(currentPage == 0
                     ? "assets/images/home_active.png"
@@ -185,7 +208,12 @@ class _BuyPageState extends ConsumerState<BuyPage> {
                   setState(() {
                     currentPage = 1;
                   });
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(page: 1,)));
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => HomePage(
+                                page: 1,
+                              )));
                 },
                 child: Image.asset(currentPage == 1
                     ? "assets/images/bell_active.png"
@@ -196,7 +224,12 @@ class _BuyPageState extends ConsumerState<BuyPage> {
                   setState(() {
                     currentPage = 2;
                   });
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(page: 2,)));
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => HomePage(
+                                page: 2,
+                              )));
                 },
                 child: Padding(
                   padding: const EdgeInsets.only(left: 60),
@@ -210,7 +243,12 @@ class _BuyPageState extends ConsumerState<BuyPage> {
                   setState(() {
                     currentPage = 3;
                   });
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(page: 3,)));
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => HomePage(
+                                page: 3,
+                              )));
                 },
                 child: Image.asset(currentPage == 3
                     ? "assets/images/settings_active.png"
@@ -231,7 +269,10 @@ class _BuyPageState extends ConsumerState<BuyPage> {
               elevation: 0,
               backgroundColor: const Color(0xFFF1C40F),
               onPressed: () {},
-              child: const Icon(Icons.add, size: 40,),
+              child: const Icon(
+                Icons.add,
+                size: 40,
+              ),
             ),
           ),
         ),
--- lib/home/presentation/pages/cart_page.dart
+++ lib/home/presentation/pages/cart_page.dart
@@ -1,13 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shopping_app/home/presentation/pages/settings_page.dart';
 
-class CartPage extends StatelessWidget {
+import 'home_page.dart';
+import 'notif_page.dart';
+
+class CartPage extends StatefulWidget {
   const CartPage({Key? key}) : super(key: key);
 
   @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  int currentPage = 2;
+
+  final screens = [
+    HomeWidget(),
+    NotifPage(),
+    CartPage(),
+    SettingsPage(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          padding: const EdgeInsets.only(left: 20),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: SizedBox(
+              width: 38,
+              height: 38,
+              child: Image.asset(
+                "assets/images/arrow-left-circle.png",
+              )),
+        ),
+        title: Text(
+          "Cart",
+          style: GoogleFonts.montserrat(
+              fontWeight: FontWeight.w700,
+              fontSize: 22,
+              color: const Color(0xFFF1C40F)),
+        ),
+        centerTitle: true,
+        actions: [
+          Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Image.asset("assets/images/search_big.png")
+          )
+        ],
+      ),
       body: Center(
         child: Text("Cart page"),
+      ),
+      bottomNavigationBar: SizedBox(
+        height: 88,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 30, right: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    currentPage = 0;
+                  });
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(page: 0,)));
+                },
+                child: Image.asset(currentPage == 0
+                    ? "assets/images/home_active.png"
+                    : "assets/images/home_icon.png"),
+              ),
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    currentPage = 1;
+                  });
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(page: 1,)));
+                },
+                child: Image.asset(currentPage == 1
+                    ? "assets/images/bell_active.png"
+                    : "assets/images/bell_icon.png"),
+              ),
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    currentPage = 2;
+                  });
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(page: 2,)));
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 60),
+                  child: Image.asset(currentPage == 2
+                      ? "assets/images/shop_active.png"
+                      : "assets/images/shop_icon.png"),
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  setState(() {
+                    currentPage = 3;
+                  });
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(page: 3,)));
+                },
+                child: Image.asset(currentPage == 3
+                    ? "assets/images/settings_active.png"
+                    : "assets/images/settings_icon.png"),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(top: 40),
+        child: SizedBox(
+          width: 60,
+          height: 60,
+          child: FittedBox(
+            child: FloatingActionButton(
+              elevation: 0,
+              backgroundColor: const Color(0xFFF1C40F),
+              onPressed: () {},
+              child: const Icon(Icons.add, size: 40,),
+            ),
+          ),
+        ),
       ),
     );
   }
