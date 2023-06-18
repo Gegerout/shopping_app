b60d155706f65f1651044b165c1dbf66cdc804c6
Gegerout <selimchik06@gmail.com>
2023-06-18 20:47:00 +0300

    home screen update

--- lib/home/presentation/pages/settings_page.dart
+++ lib/home/presentation/pages/settings_page.dart
@@ -1,14 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Settings page"),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: IconButton(
+            padding: const EdgeInsets.only(left: 20),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: SizedBox(
+                width: 38,
+                height: 38,
+                child: Image.asset(
+                  "assets/images/arrow-left-circle.png",
+                )),
+          ),
+          title: Text(
+            "Settings",
+            style: GoogleFonts.montserrat(
+                fontWeight: FontWeight.w700,
+                fontSize: 22,
+                color: const Color(0xFFF1C40F)),
+          ),
+          centerTitle: true,
+          actions: [
+            Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: Image.asset("assets/images/search_big.png"))
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(left: 30, right: 30),
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: 66,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      alignment: Alignment.centerLeft,
+                      backgroundColor: const Color(0xFFC4C4C4).withOpacity(0.35),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30)
+                      )
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 60),
+                      child: Text(
+                        "Your account",
+                        style: GoogleFonts.montserrat(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                            color: Colors.black.withOpacity(0.5)),
+                      ),
+                    )),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 66,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        alignment: Alignment.centerLeft,
+                        backgroundColor: const Color(0xFFC4C4C4).withOpacity(0.35),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)
+                        )
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 60),
+                      child: Text(
+                        "Your order",
+                        style: GoogleFonts.montserrat(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                            color: Colors.black.withOpacity(0.5)),
+                      ),
+                    )),
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 66,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        alignment: Alignment.centerLeft,
+                        backgroundColor: const Color(0xFFE74C3C).withOpacity(0.6),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)
+                        )
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 60),
+                      child: Text(
+                        "Sign out",
+                        style: GoogleFonts.montserrat(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                            color: Colors.white),
+                      ),
+                    )),
+              )
+            ],
+          ),
+        ));
   }
 }
--- lib/home/presentation/pages/success_page.dart
+++ lib/home/presentation/pages/success_page.dart
@@ -1,8 +1,51 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shopping_app/home/presentation/pages/home_page.dart';
 
-class SuccessPage extends StatelessWidget {
+class SuccessPage extends StatefulWidget {
   const SuccessPage({Key? key}) : super(key: key);
+
+  @override
+  State<SuccessPage> createState() => _SuccessPageState();
+}
+
+class _SuccessPageState extends State<SuccessPage> {
+  late Timer _timer;
+  int _start = 5;
+
+  void startTimer() {
+    const oneSec = Duration(seconds: 1);
+    _timer = Timer.periodic(
+      oneSec,
+          (Timer timer) {
+        if (_start == 0) {
+          setState(() {
+            timer.cancel();
+          });
+          Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage(page: 0)));
+        } else {
+          setState(() {
+            _start--;
+          });
+        }
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
